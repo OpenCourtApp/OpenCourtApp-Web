ead30f4f03f63ba3feeae26e2435610357e1b230
JOÃO PEDRO CREPALDI GONÇALVES <joao.goncalves231@etec.sp.gov.br>
2026-03-30 18:18:33 -0300

    feat: início da estrutura tela de login

--- src/app/(auth)/login/page.js
+++ src/app/(auth)/login/page.js
@@ -1,8 +1,27 @@
-export default function loginScreen(){
+export default function loginScreen() {
     return (
         <div>
-            <main>
-                login screen
+            <main>//parte esquerda
+                <div>logo</div>
+
+                <div>
+                    <div>
+                        <text>Schedule courts.</text>
+                        <text>Eliminate confusion.</text>
+                    </div>
+
+                    <text>A centralized booking plataform for schools. Know exactly who reserved the court and when - with zero overlap.</text>
+                </div>
+
+                <div>
+                    <text>Real-time booking visibility</text>
+                    <text>Role-based access control</text>
+                    <text>Authorized collaborators only</text>
+                </div>
+            </main>
+
+            <main>//parte direita
+                
             </main>
         </div>
     );
