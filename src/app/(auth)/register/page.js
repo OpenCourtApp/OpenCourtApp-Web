0136279373171b91789e452ef2fcd57a0de51984
BRENO VALENTIM BERNARDO <breno.bernardo01@etec.sp.gov.br>
2026-04-16 13:49:42 -0300

    w.i.p - Criação da tela de register

--- src/app/(auth)/register/page.js
+++ src/app/(auth)/register/page.js
@@ -1,8 +1,15 @@
-export default function registerScreen(){
+'use client';
+
+import styles from './register.module.css';
+
+export default function registerScreen() {
     return (
-        <div>
+        <div className={styles.container}>
             <main>
-                singUp screen
+                <div className={styles.containerModal}>
+                    <img src="/teste-logomarca.svg" alt="Logomarca" className={styles.logomarca} />
+                    <p>Teste</p>
+                </div>
             </main>
         </div>
     );
