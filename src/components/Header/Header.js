397d1042bb66706ae75b09dad6a0e672989684fc
Baumzin <brenosuvale@gmail.com>
2026-05-01 14:47:28 -0300

    feat - Adição de subtítulo ao Header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,12 +2,13 @@
 
 import styles from './header.module.css';
 
-export function Header({ title, children, action }) {
+export function Header({ title, subtitle, children, action }) {
 
     return (
         <header className={styles.header}>
             <div className={styles.headerTitle}>
                 <h1 className={styles.title}>{title}</h1>
+                <p className={styles.subtitle}>{subtitle}</p>
             </div>
             <div className={styles.headerContent}>
                 {children}
