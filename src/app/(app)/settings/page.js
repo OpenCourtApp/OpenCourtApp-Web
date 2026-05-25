58302402e8f98d99b31b995eb227da57ca691444
BRENO VALENTIM BERNARDO <breno.bernardo01@etec.sp.gov.br>
2026-05-25 15:28:35 -0300

    w.i.p - Implementação de conteúdo em Settings

--- src/app/(app)/settings/page.js
+++ src/app/(app)/settings/page.js
@@ -15,9 +15,20 @@ export default function SettingsPage() {
                     subtitle="Manage your account"
                 />
                 <div className={styles.modalContainer}>
-                    <div className={styles.titleContainer}>
-                        <h1 className={styles.titleProfile}>Profile information</h1>
+                    <h1 className={styles.titleProfile}>Profile information</h1>
+                    <div className={styles.row}>
+                        <h5 className={styles.label}>Full name</h5>
+                        <h5 className={styles.label}>Email Address</h5>
                     </div>
+                    <div className={styles.rowInput}>
+                        <input className={styles.input}/>
+                        <input className={styles.input}/>
+                    </div>
+                    <h5 className={styles.label}>New Password</h5>
+                    <input className={styles.input}/>
+                    <Button className={styles.btnSaveChanges}>Save Changes</Button>
+                </div>
+                <div className={styles.modalContainer}>
 
                 </div>
             </main>
